be5195f409ace221024fe7603f5116b044677ed4
andaluciacurtis <143487070+andaluciacurtis@users.noreply.github.com>
2023-12-02 04:41:35 +0000

    Added conditions for decimal points

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const keyboard = document.querySelector(".keyboard");
 const screen = document.querySelector(".screen");
 const keys = [7, 8, 9, "del", 4, 5, 6, "+", 1, 2, 3, "-", ".", "0", "/", "*", "reset", "="];
 
-let currentPhrase = "";
-let visiblePhrase = "";
+let currentPhrase = "0";
 let lastKey = "";
+let isDecimal = false;
 
 createKeyboard();
 
@@ -42,32 +42,42 @@ function createKeyboard() {
 
 function updateKeyboard(currentKey) {
   if (currentKey == "=") {
+    currentPhrase = Math.round(eval(currentPhrase) * 100000) / 100000;
+  } else if (currentKey === "reset") {
+    currentPhrase = "0";
+  } else if (currentKey === "del") {
+    currentPhrase = currentPhrase.slice(0, -1);
+    lastKey = currentPhrase.charAt(currentPhrase.length - 1);
+    console.log(lastKey);
+  } else if (currentKey === ".") {
+    if (currentPhrase.includes(".") && isDecimal) {
+      return;
+    } else if (isNaN(currentPhrase.charAt(currentPhrase.length - 1))) {
+      currentPhrase += "0.";
+      isDecimal = true;
+    } else if (lastKey === "=") {
+      currentPhrase = "0.";
+      isDecimal = true;
+    } else {
+      currentPhrase += ".";
+      isDecimal = true;
+    }
+  } else if (isNaN(currentKey)) {
     currentPhrase = eval(currentPhrase);
-    visiblePhrase = currentPhrase;
-    lastKey = currentPhrase;
-  } else {
-    if (currentKey === "reset") {
-      currentPhrase = "";
-      visiblePhrase = "";
-    } else if (currentKey === "del") {
-      currentPhrase = currentPhrase.slice(0, -1);
-      lastKey = currentPhrase.charAt(currentPhrase.length - 1);
-      console.log(lastKey);
-      visiblePhrase = currentPhrase;
-    } else if (isNaN(currentKey)) {
-      currentPhrase = eval(currentPhrase);
+    currentPhrase += currentKey;
+    
+    // ensures that two decimal points aren't put in the same number
+    if (isNaN(currentPhrase.charAt(currentPhrase.length - 1)) || !currentPhrase.includes(".")) {
+      isDecimal = false;
+    }
+  } else { // number
+    if (lastKey === "=" || lastKey === "reset" || currentPhrase == "0") {
+      // clears and starts with a new number
+      currentPhrase = currentKey;
+    } else {
       currentPhrase += currentKey;
-      visiblePhrase = currentPhrase;
-    } else { // number
-      if (!isNaN(lastKey)) {
-        // clears and starts with a new number
-        currentPhrase = currentKey;
-      } else {
-        currentPhrase += currentKey;
-      }
-      visiblePhrase = currentPhrase;
     }
-    lastKey = currentKey;
-    }
-  screen.innerHTML = `<p>${visiblePhrase}</p>`;
+  }  
+  lastKey = currentKey;
+  screen.innerHTML = `<p>${currentPhrase}</p>`;
 }
